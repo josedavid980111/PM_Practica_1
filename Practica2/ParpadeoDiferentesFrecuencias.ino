0290502a5367bcaefeea86180567cc6217178fa3
josedavid980111 <35538108+josedavid980111@users.noreply.github.com>
2020-03-03 14:46:14 -0600

    .

---
+++ Practica2/ParpadeoDiferentesFrecuencias.ino
@@ -0,0 +1,157 @@
+void setup()
+{
+DDRB = DDRB | B00111100; // Data Direction Register B: Inputs 0-6, Output 7
+}
+
+void loop()
+{
+
+asm (
+"inicio: \n\t" 
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"sbi 0x05,0x03 \n\t"
+"sbi 0x05,0x02 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"cbi 0x05,0x03 \n\t"
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"cbi 0x05,0x02 \n\t"
+"sbi 0x05,0x03 \n\t"
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"cbi 0x05,0x03 \n\t"
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"sbi 0x05,0x02 \n\t"
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"sbi 0x05,0x03 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"cbi 0x05,0x03 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+
+
+"cbi 0x05,0x02 \n\t"
+"sbi 0x05,0x03 \n\t"
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"cbi 0x05,0x03 \n\t"
+"sbi 0x05,0x05 \n\t"
+"sbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+"sbi 0x05,0x05 \n\t"
+"cbi 0x05,0x04 \n\t"
+"call tiempo \n\t"
+"cbi 0x05,0x05 \n\t"
+"call tiempo \n\t"
+
+
+
+
+"jmp main \n\t"
+
+
+
+"tiempo: \n\t"
+"LDI r22, 20 \n\t"
+"LOOP_3: \n\t"
+"LDI r21, 255 \n\t"
+"LOOP_2: \n\t"
+"LDI r20, 255 \n\t"
+"LOOP_1: \n\t"
+"DEC r20 \n\t"
+"BRNE LOOP_1 \n\t"
+"DEC r21 \n\t"
+"BRNE LOOP_2 \n\t"
+"DEC r22 \n\t"
+"BRNE LOOP_3 \n\t"
+"ret \n\t"
+);
+}
